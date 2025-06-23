7953dbb7cf1834160a8d2bcf04ff4e52a580bc16
Danilo Naves <danilonaves2209@gmail.com>
2025-06-23 17:43:50 -0300

    feat: Modularização

---
+++ include/buzzer/buzzer_modulo.h
@@ -0,0 +1,7 @@
+#ifndef BUZZER_MODULO_H
+#define BUZZER_MODULO_H
+
+void pwm_init_buzzer();
+void play_buzzer(int duration_ms);
+
+#endif 
---
+++ include/config.h
@@ -0,0 +1,19 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// ==========================================================
+// CONFIGURAÇÕES GERAIS DO PROJETO
+// ==========================================================
+
+// Pinagem
+#define BUZZER_PIN 21
+#define BUTTON_A 5
+#define BUTTON_B 6
+#define LED_VERDE_PIN 11
+#define LED_AZUL_PIN 11
+#define LED_VERMELHO_PIN 13
+#define I2C_PORT i2c1
+#define I2C_SDA_PIN 14
+#define I2C_SCL_PIN 15
+
+#endif // CONFIG_H
